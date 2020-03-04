89892e737bb3379ed83e2d8082268654f95e47b7
zhangshuang <zhangshuang3@jd.com>
2020-03-04 12:18:52 +0800

    add node start frame

---
+++ src/test/leaderchange/HeartBeatTest.java
@@ -0,0 +1,58 @@
+package test.leaderchange;
+
+import bftsmart.tom.AsynchServiceProxy;
+import bftsmart.tom.ServiceReplica;
+
+import java.util.Random;
+
+/**
+ * @Author: zhangshuang
+ * @Date: 2020/3/4 11:33 AM
+ * Version 1.0
+ */
+public class HeartBeatTest {
+
+    private static int nodeNums = 4;
+
+
+    public static void main(String[] args) {
+
+        if (Integer.parseInt(args[0]) < 4) {
+            System.out.println("Client proc id error, cann't same with node server!!");
+            return;
+        }
+
+        int clientProcId = Integer.parseInt(args[0]);
+
+        // create client request content
+        Random random = new Random();
+        byte[] bytes = new byte[4];
+        random.nextBytes(bytes);
+
+        ServiceReplica[] replicas;
+
+        //start 4 node servers
+        for (int i = 0; i < nodeNums ; i++) {
+           new NodeServerTest(i);
+        }
+
+        //create client proxy
+        AsynchServiceProxy clientProxy = new AsynchServiceProxy(clientProcId);
+
+        clientProxy.invokeOrdered(bytes);
+
+        // test1
+        // TODO: 2020/3/4  
+        
+        // test2
+        // TODO: 2020/3/4  
+
+
+    }
+
+
+
+
+
+
+}
---
+++ src/test/leaderchange/NodeServerTest.java
@@ -0,0 +1,155 @@
+package test.leaderchange;
+
+import bftsmart.consensus.app.BatchAppResultImpl;
+import bftsmart.demo.counter.CounterServer;
+import bftsmart.tom.MessageContext;
+import bftsmart.tom.ReplyContextMessage;
+import bftsmart.tom.ServiceReplica;
+import bftsmart.tom.server.defaultservices.DefaultRecoverable;
+import bftsmart.tom.server.defaultservices.DefaultReplier;
+import bftsmart.tom.util.BytesUtils;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * @Author: zhangshuang
+ * @Date: 2020/3/4 11:50 AM
+ * Version 1.0
+ */
+public class NodeServerTest extends DefaultRecoverable {
+    private int counter = 0;
+    private int iterations = 0;
+
+    ServiceReplica replica = null;
+
+    public NodeServerTest(int id) {
+        replica = new ServiceReplica(id, "config", this, this, null, new DefaultReplier());
+    }
+
+    @Override
+    public byte[][] appExecuteBatch(byte[][] commands, MessageContext[] msgCtxs, boolean fromConsensus) {
+
+
+        byte [][] replies = new byte[commands.length][];
+        for (int i = 0; i < commands.length; i++) {
+            if(msgCtxs != null && msgCtxs[i] != null) {
+                replies[i] = executeSingle(commands[i],msgCtxs[i]);
+            }
+            else executeSingle(commands[i],null);
+        }
+
+        return replies;
+    }
+
+    @Override
+    public byte[][] appExecuteBatch(byte[][] commands, MessageContext[] msgCtxs, boolean fromConsensus, List<ReplyContextMessage> replyContextMessages) {
+        return appExecuteBatch(commands, msgCtxs, fromConsensus);
+    }
+
+    @Override
+    public byte[] appExecuteUnordered(byte[] command, MessageContext msgCtx) {
+
+        iterations++;
+        System.out.println("(" + iterations + ") Reading counter at value: " + counter);
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream(4);
+            new DataOutputStream(out).writeInt(counter);
+            return out.toByteArray();
+        } catch (IOException ex) {
+            System.err.println("Invalid request received!");
+            return new byte[0];
+        }
+    }
+
+    private byte[] executeSingle(byte[] command, MessageContext msgCtx) {
+        iterations++;
+        try {
+            int increment = new DataInputStream(new ByteArrayInputStream(command)).readInt();
+            //System.out.println("read-only request: "+(msgCtx.getConsensusId() == -1));
+            counter += increment;
+
+            if (msgCtx != null) {
+                if (msgCtx.getConsensusId() == -1) {
+                    System.out.println("(" + iterations + ") Counter was incremented: " + counter);
+                } else {
+                    System.out.println("(" + iterations + " / " + msgCtx.getConsensusId() + ") Counter was incremented: " + counter);
+                }
+            }
+            else {
+                System.out.println("(" + iterations + ") Counter was incremented: " + counter);
+            }
+            ByteArrayOutputStream out = new ByteArrayOutputStream(4);
+            new DataOutputStream(out).writeInt(counter);
+            return out.toByteArray();
+        } catch (IOException ex) {
+            System.err.println("Invalid request received!");
+            return new byte[0];
+        }
+    }
+
+    public static void main(String[] args){
+        if(args.length < 1) {
+            System.out.println("Use: java CounterServer <processId>");
+            System.exit(-1);
+        }
+        new NodeServerTest(Integer.parseInt(args[0]));
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void installSnapshot(byte[] state) {
+        try {
+            System.out.println("setState called");
+            ByteArrayInputStream bis = new ByteArrayInputStream(state);
+            ObjectInput in = new ObjectInputStream(bis);
+            counter =  in.readInt();
+            in.close();
+            bis.close();
+        } catch (Exception e) {
+            System.err.println("[ERROR] Error deserializing state: "
+                    + e.getMessage());
+        }
+    }
+
+    @Override
+    public byte[] getSnapshot() {
+        try {
+            System.out.println("getState called");
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutput out = new ObjectOutputStream(bos);
+            out.writeInt(counter);
+            out.flush();
+            bos.flush();
+            out.close();
+            bos.close();
+            return bos.toByteArray();
+        } catch (IOException ioe) {
+            System.err.println("[ERROR] Error serializing state: "
+                    + ioe.getMessage());
+//			return "ERROR".getBytes();
+            return BytesUtils.getBytes("ERROR");
+        }
+    }
+
+    @Override
+    public BatchAppResultImpl preComputeAppHash(byte[][] commands) {
+        return null;
+    }
+
+    @Override
+    public List<byte[]> updateAppResponses(List<byte[]> asyncResponseLinkedList, byte[] commonHash, boolean isConsistent) {
+        return null;
+    }
+
+    @Override
+    public void preComputeAppCommit(String batchId) {
+
+    }
+
+    @Override
+    public void preComputeAppRollback(String batchId) {
+
+    }
+}
