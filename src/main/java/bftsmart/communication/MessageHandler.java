b7f3fff5cc0d731337d756b0788fd8864af96c3a
shaozhuguang <shaozhuguang@jd.com>
2020-04-02 11:25:57 +0800

    change message handler to thread pools

--- src/main/java/bftsmart/communication/MessageHandler.java
+++ src/main/java/bftsmart/communication/MessageHandler.java
@@ -39,6 +39,8 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -50,7 +52,10 @@ public class MessageHandler {
     private TOMLayer tomLayer;
     //private Cipher cipher;
     private Mac mac;
-    
+    private final ExecutorService conesnsusMsgThreadPool = Executors.newSingleThreadExecutor();
+    private final ExecutorService heartMsgThreadPool = Executors.newSingleThreadExecutor();
+    private final ExecutorService lcMsgThreadPool = Executors.newSingleThreadExecutor();
+
     public MessageHandler() {
         try {
             //this.cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
@@ -68,27 +73,38 @@ public class MessageHandler {
     }
 
     @SuppressWarnings("unchecked")
-    public void processData(SystemMessage sm) {
+    public void processData(final SystemMessage sm) {
         if (sm instanceof ConsensusMessage) {
-            
+            final ConsensusMessage consMsg = (ConsensusMessage) sm;
+            processConsensusMsg(consMsg);
+        } else if (sm instanceof HeartBeatMessage || sm instanceof LeaderRequestMessage || sm instanceof LeaderResponseMessage) {
+            processHeartMsg(sm);
+        } else {
+        	processLCMsg(sm);
+        }
+    }
+
+    private void processConsensusMsg(final ConsensusMessage consMsg) {
+
+        // 由共识消息处理线程单独处理
+        conesnsusMsgThreadPool.execute(() -> {
+
             int myId = tomLayer.controller.getStaticConf().getProcessId();
-            
-            ConsensusMessage consMsg = (ConsensusMessage) sm;
 
             System.out.printf("(MessageHandler) node %s receive consensus msg from %s , type is = %s, time = %s \r\n",
-                    tomLayer.controller.getStaticConf().getProcessId(), sm.getSender(), consMsg.getType(), System.currentTimeMillis());
+                    tomLayer.controller.getStaticConf().getProcessId(), consMsg.getSender(), consMsg.getType(), System.currentTimeMillis());
 
             if (tomLayer.controller.getStaticConf().getUseMACs() == 0 || consMsg.authenticated || consMsg.getSender() == myId) acceptor.deliver(consMsg);
             else if (consMsg.getType() == MessageFactory.ACCEPT && consMsg.getProof() != null) {
-                                        
+
                 //We are going to verify the MAC vector at the algorithm level
                 HashMap<Integer, byte[]> macVector = (HashMap<Integer, byte[]>) consMsg.getProof();
-                               
+
                 byte[] recvMAC = macVector.get(myId);
-                
+
                 ConsensusMessage cm = new ConsensusMessage(MessageFactory.ACCEPT,consMsg.getNumber(),
                         consMsg.getEpoch(), consMsg.getSender(), consMsg.getValue());
-                
+
                 ByteArrayOutputStream bOut = new ByteArrayOutputStream(248);
                 try {
                     new ObjectOutputStream(bOut).writeObject(cm);
@@ -97,106 +113,115 @@ public class MessageHandler {
                 }
 
                 byte[] data = bOut.toByteArray();
-        
-                //byte[] hash = tomLayer.computeHash(data); 
-                
+
+                //byte[] hash = tomLayer.computeHash(data);
+
                 byte[] myMAC = null;
-                
+
                 /*byte[] k = tomLayer.getCommunication().getServersConn().getSecretKey(paxosMsg.getSender()).getEncoded();
                 SecretKeySpec key = new SecretKeySpec(new String(k).substring(0, 8).getBytes(), "DES");*/
-                
+
                 SecretKey key = tomLayer.getCommunication().getServersConn().getSecretKey(consMsg.getSender());
                 try {
-                    this.mac.init(key);                   
+                    this.mac.init(key);
                     myMAC = this.mac.doFinal(data);
                 } catch (/*IllegalBlockSizeException | BadPaddingException |*/ InvalidKeyException ex) {
                     ex.printStackTrace();
                 }
-                
+
                 if (recvMAC != null && myMAC != null && Arrays.equals(recvMAC, myMAC))
                     acceptor.deliver(consMsg);
                 else {
-                    Logger.println("(MessageHandler.processData) WARNING: invalid MAC from " + sm.getSender());
-                    System.out.println("(MessageHandler.processData) WARNING: invalid MAC from " + sm.getSender());
+                    Logger.println("(MessageHandler.processData) WARNING: invalid MAC from " + consMsg.getSender());
+                    System.out.println("(MessageHandler.processData) WARNING: invalid MAC from " + consMsg.getSender());
+                }
+            } else {
+                System.out.println("(MessageHandler.processData) Discarding unauthenticated message from " + consMsg.getSender());
+                Logger.println("(MessageHandler.processData) Discarding unauthenticated message from " + consMsg.getSender());
+            }
+        });
+    }
+
+    private void processHeartMsg(final SystemMessage sm) {
+        heartMsgThreadPool.execute(() -> {
+            if (sm instanceof HeartBeatMessage) {
+                // 心跳消息
+                System.out.printf("(MessageHandler) node %s receive heart beat from %s , time = %s \r\n",
+                        tomLayer.controller.getStaticConf().getProcessId(), ((HeartBeatMessage)sm).getLeader(), System.currentTimeMillis());
+
+                tomLayer.heartBeatTimer.receiveHeartBeatMessage((HeartBeatMessage)sm);
+            } else if (sm instanceof LeaderRequestMessage) {
+                // 获取Leader节点请求的消息
+                tomLayer.heartBeatTimer.receiveLeaderRequestMessage((LeaderRequestMessage) sm);
+            } else if (sm instanceof LeaderResponseMessage) {
+                // 获取Leader节点请求的消息
+                tomLayer.heartBeatTimer.receiveLeaderResponseMessage((LeaderResponseMessage) sm);
+            }
+        });
+    }
+
+    private void processLCMsg(final SystemMessage sm) {
+        lcMsgThreadPool.execute(() -> {
+            if (tomLayer.controller.getStaticConf().getUseMACs() == 0 || sm.authenticated) {
+                /*** This is Joao's code, related to leader change */
+                if (sm instanceof LCMessage) {
+                    LCMessage lcMsg = (LCMessage) sm;
+
+                    String type = null;
+                    switch(lcMsg.getType()) {
+
+                        case TOMUtil.STOP:
+                            type = "STOP";
+                            break;
+                        case TOMUtil.STOPDATA:
+                            type = "STOPDATA";
+                            break;
+                        case TOMUtil.SYNC:
+                            type = "SYNC";
+                            break;
+                        default:
+                            type = "LOCAL";
+                            break;
+                    }
+
+//	                System.out.println("(MessageHandler.processData) LC_MSG received: type " + type + ", regency " + lcMsg.getReg() + ", (replica " + lcMsg.getSender() + ")");
+                    if (lcMsg.TRIGGER_LC_LOCALLY) tomLayer.requestsTimer.run_lc_protocol();
+                    else tomLayer.getSynchronizer().deliverTimeoutRequest(lcMsg);
+                    /**************************************************************/
+
+                } else if (sm instanceof ForwardedMessage) {
+                    TOMMessage request = ((ForwardedMessage) sm).getRequest();
+                    tomLayer.requestReceived(request);
+
+                    /** This is Joao's code, to handle state transfer */
+                } else if (sm instanceof SMMessage) {
+                    SMMessage smsg = (SMMessage) sm;
+                    // System.out.println("(MessageHandler.processData) SM_MSG received: type " + smsg.getType() + ", regency " + smsg.getRegency() + ", (replica " + smsg.getSender() + ")");
+                    switch(smsg.getType()) {
+                        case TOMUtil.SM_REQUEST:
+                            tomLayer.getStateManager().SMRequestDeliver(smsg, tomLayer.controller.getStaticConf().isBFT());
+                            break;
+                        case TOMUtil.SM_REPLY:
+                            tomLayer.getStateManager().SMReplyDeliver(smsg, tomLayer.controller.getStaticConf().isBFT());
+                            break;
+                        case TOMUtil.SM_ASK_INITIAL:
+                            tomLayer.getStateManager().currentConsensusIdAsked(smsg.getSender());
+                            break;
+                        case TOMUtil.SM_REPLY_INITIAL:
+                            tomLayer.getStateManager().currentConsensusIdReceived(smsg);
+                            break;
+                        default:
+                            tomLayer.getStateManager().stateTimeout();
+                            break;
+                    }
+                    /******************************************************************/
+                } else {
+                    System.out.println("UNKNOWN MESSAGE TYPE: " + sm);
                 }
             } else {
                 System.out.println("(MessageHandler.processData) Discarding unauthenticated message from " + sm.getSender());
-                Logger.println("(MessageHandler.processData) Discarding unauthenticated message from " + sm.getSender());
             }
-
-        } else if (sm instanceof HeartBeatMessage) {
-            // 心跳消息
-            System.out.printf("(MessageHandler) node %s receive heart beat from %s , time = %s \r\n",
-                    tomLayer.controller.getStaticConf().getProcessId(), ((HeartBeatMessage)sm).getLeader(), System.currentTimeMillis());
-
-            tomLayer.heartBeatTimer.receiveHeartBeatMessage((HeartBeatMessage)sm);
-        } else if (sm instanceof LeaderRequestMessage) {
-            // 获取Leader节点请求的消息
-            tomLayer.heartBeatTimer.receiveLeaderRequestMessage((LeaderRequestMessage) sm);
-        } else if (sm instanceof LeaderResponseMessage) {
-            // 获取Leader节点请求的消息
-            tomLayer.heartBeatTimer.receiveLeaderResponseMessage((LeaderResponseMessage) sm);
-        } else {
-        	if (tomLayer.controller.getStaticConf().getUseMACs() == 0 || sm.authenticated) {
-	            /*** This is Joao's code, related to leader change */
-	            if (sm instanceof LCMessage) {
-	                LCMessage lcMsg = (LCMessage) sm;
-	
-	                String type = null;
-	                switch(lcMsg.getType()) {
-	
-	                    case TOMUtil.STOP:
-	                        type = "STOP";
-	                        break;
-	                    case TOMUtil.STOPDATA:
-	                        type = "STOPDATA";
-	                        break;
-	                    case TOMUtil.SYNC:
-	                        type = "SYNC";
-	                        break;
-	                    default:
-	                        type = "LOCAL";
-	                        break;
-	                }
-	
-//	                System.out.println("(MessageHandler.processData) LC_MSG received: type " + type + ", regency " + lcMsg.getReg() + ", (replica " + lcMsg.getSender() + ")");
-	                if (lcMsg.TRIGGER_LC_LOCALLY) tomLayer.requestsTimer.run_lc_protocol();
-	                else tomLayer.getSynchronizer().deliverTimeoutRequest(lcMsg);
-	            /**************************************************************/
-	
-	            } else if (sm instanceof ForwardedMessage) {
-	                TOMMessage request = ((ForwardedMessage) sm).getRequest();
-	                tomLayer.requestReceived(request);
-	
-	            /** This is Joao's code, to handle state transfer */
-	            } else if (sm instanceof SMMessage) {
-	                SMMessage smsg = (SMMessage) sm;
-	                // System.out.println("(MessageHandler.processData) SM_MSG received: type " + smsg.getType() + ", regency " + smsg.getRegency() + ", (replica " + smsg.getSender() + ")");
-	                switch(smsg.getType()) {
-	                    case TOMUtil.SM_REQUEST:
-		                    tomLayer.getStateManager().SMRequestDeliver(smsg, tomLayer.controller.getStaticConf().isBFT());
-	                        break;
-	                    case TOMUtil.SM_REPLY:
-		                    tomLayer.getStateManager().SMReplyDeliver(smsg, tomLayer.controller.getStaticConf().isBFT());
-	                        break;
-	                    case TOMUtil.SM_ASK_INITIAL:
-	                    	tomLayer.getStateManager().currentConsensusIdAsked(smsg.getSender());
-	                    	break;
-	                    case TOMUtil.SM_REPLY_INITIAL:
-	                    	tomLayer.getStateManager().currentConsensusIdReceived(smsg);
-	                    	break;
-	                    default:
-		                    tomLayer.getStateManager().stateTimeout();
-	                        break;
-	                }
-	            /******************************************************************/
-	            } else {
-	            	System.out.println("UNKNOWN MESSAGE TYPE: " + sm);
-	            }
-	        } else {
-	            System.out.println("(MessageHandler.processData) Discarding unauthenticated message from " + sm.getSender());
-	        }
-        }
+        });
     }
     
     protected void verifyPending() {
