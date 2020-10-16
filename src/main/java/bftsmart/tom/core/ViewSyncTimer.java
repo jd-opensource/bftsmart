bd47ec0aa3d60f3301e25a1b73bd54f8b1ac2daa
shaozhuguang <shaozhuguang@jd.com>
2020-10-16 13:21:52 +0800

    delete some import

--- src/main/java/bftsmart/tom/core/ViewSyncTimer.java
+++ src/main/java/bftsmart/tom/core/ViewSyncTimer.java
@@ -3,12 +3,9 @@ package bftsmart.tom.core;
 import bftsmart.reconfiguration.views.NodeNetwork;
 import bftsmart.reconfiguration.views.View;
 import bftsmart.tom.core.messages.ViewMessage;
-import bftsmart.tom.leaderchange.TimestampMessage;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
