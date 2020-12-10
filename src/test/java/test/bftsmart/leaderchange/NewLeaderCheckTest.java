8189a1dfa6f48cb872ba4bf7e9ce49af849101ba
huanghaiquan <huanghaiquan@jd.com>
2020-12-10 23:25:25 +0800

    refactored HeartBeatTimer to do unit test;

---
+++ src/test/java/test/bftsmart/leaderchange/NewLeaderCheckTest.java
@@ -0,0 +1,17 @@
+package test.bftsmart.leaderchange;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import bftsmart.tom.leaderchange.HeartBeatTimer;
+
+public class NewLeaderCheckTest {
+
+	@Test
+	public void test() {
+//		HeartBeatTimer.newLeaderCheck(leaderResponseMessages, f);
+//		fail("Not yet implemented");
+	}
+
+}
