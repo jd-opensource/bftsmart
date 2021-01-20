8f9155fc27e97626acd6280e03ebfbb868a00300
huanghaiquan <huanghaiquan@jd.com>
2021-01-20 09:23:31 +0800

    added test case of ServerCommunicationLayer;

---
+++ src/test/java/test/bftsmart/communication/server/ServerCommunicationLayerTest.java
@@ -0,0 +1,14 @@
+package test.bftsmart.communication.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerCommunicationLayerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/test/bftsmart/communication/server/socket/SocketServerCommunicationLayerTest.java
@@ -0,0 +1,14 @@
+package test.bftsmart.communication.server.socket;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SocketServerCommunicationLayerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
